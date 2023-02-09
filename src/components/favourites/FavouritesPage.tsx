92583dfca95b29d3a71e7e7a82a53ce1ffcea212
GreRin <rigor1988@gmail.com>
2023-02-09 22:33:10 +0100

    feat: implemented GrapghQl queries

--- src/components/favourites/FavouritesPage.tsx
+++ src/components/favourites/FavouritesPage.tsx
@@ -1,24 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { useAppSelector } from '../../hooks/redux';
-import { useQuery } from '@apollo/client';
-import { GET_ALL_COURSES } from '../../query/Courses';
+import { getCourseById, getCourses } from '../../query/Courses';
 import CourseCard from './CourseCard';
 
 export const FavouritesPage = (): any => {
   const { favourites } = useAppSelector((state) => state.github);
-  const { data, loading, error } = useQuery(GET_ALL_COURSES);
-
   const [courses, setCourses] = useState([]);
+  const [course, setCourse] = useState([]);
 
   useEffect(() => {
-    if (!loading) {
-      setCourses(data.getAllCourses);
-    }
+    getCourses().then(setCourses);
   }, []);
 
-  console.log(courses);
+  useEffect(() => {
+    getCourseById(2).then((data) => {
+      console.log(data);
+      setCourse(data);
+    });
+    console.log(course);
+  }, []);
 
-  if (favourites.length === 0) return <p className="text-center">No items.</p>;
+  if (favourites.length === 0 && courses.length === 0) return <p className="text-center">No items.</p>;
 
   return (
     <>
@@ -34,7 +36,7 @@ export const FavouritesPage = (): any => {
         </ul>
       </div>
       <div className="w-100 d-inline-flex">
-        {courses && courses.map((course, index) => <CourseCard course={course} key={index} />)}
+        {courses && courses.map((data, index) => <CourseCard course={data} key={index} />)}
       </div>
     </>
   );
--- src/query/Courses.ts
+++ src/query/Courses.ts
@@ -1,13 +1,36 @@
-import { gql } from '@apollo/client';
+import { request, gql } from 'graphql-request';
 
-export const GET_ALL_COURSES = gql`
-  query {
-    getAllCourses {
-      title
-      author
-      description
-      topic
-      url
+const GRAPHQL_URL = 'http://localhost:4000/api/graphql';
+
+export const getCourses = async (): Promise<any> => {
+  const query = gql`
+    query {
+      getAllCourses {
+        id
+        title
+        description
+      }
     }
-  }
-`;
+  `;
+
+  const data = await request(GRAPHQL_URL, query);
+  return data.getAllCourses;
+};
+
+export const getCourseById = async (id: number): Promise<any> => {
+  const query = gql`
+    query courseQuery($id: Int!) {
+      course(id: $id) {
+        id
+        title
+        description
+        topic
+        url
+      }
+    }
+  `;
+
+  const variables = { id };
+  const data = await request(GRAPHQL_URL, query, variables);
+  return data.course;
+};
